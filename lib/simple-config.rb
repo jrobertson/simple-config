0087d4d8a34b462b518d051eb09da7f2e8a709be
James Robertson <james@r0bertson.co.uk>
2020-07-06 11:11:08 +0100

    feature: Primitive data types are now detected automatically.

--- lib/simple-config.rb
+++ lib/simple-config.rb
@@ -8,9 +8,10 @@ require 'kvx'
 class SimpleConfig < Kvx
   
   
-  def initialize(x, attributes: {}, debug: false)    
+  def initialize(x, attributes: {}, auto_detect_type: true, debug: false)    
 
     @ignore_blank_lines = true
+    @auto_detect_type = auto_detect_type
     
     if x.is_a? SimpleConfig then
       @to_h = x.to_h
@@ -27,8 +28,31 @@ class SimpleConfig < Kvx
   private
   
   def parse_to_h(s)
-    super(s, header_pattern: %r(^<\?simple-?config\s*))
+    h = super(s, header_pattern: %r(^<\?simple-?config\s*))
+    @auto_detect_type ? scan2type(h) : h
   end    
+  
+  def find_type(v)
+
+    if v[/^\d+$/] and v.to_i.to_s.length == v.length then v.to_i
+    elsif v[/^\d+\.\d+$/] and v.to_f.to_s.length == v.length then v.to_f
+    elsif v.downcase[/^(?:true|false|on|off|yes|no)$/]
+      v.downcase[/^(?:true|on|yes)$/] ? true : false
+    elsif v[/^\#.*\#$/] then
+      Time.parse(v)
+    else
+      v
+    end
+
+  end
+
+  def scan2type(h)
+
+    h.each do |key, value|
+      h[key] = value.is_a?(Hash) ? scan2type(value) : find_type(value)
+    end
+
+  end  
 
 
 end
